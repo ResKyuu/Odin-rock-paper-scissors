f62e5731582ac974e063c268b87d27bdce681b23
ResKyuu <73728388+ResKyuu@users.noreply.github.com>
2025-02-26 15:39:28 +0000

    Finished the Game.
    Added the checkspelling function to make sure that the users Input is correct.
    Added checkWinner function which checks if either the user of the pc won the round.
    Added playGame function which repeatidly plays the game until User or Pc has gotten a score of 3.
    Added a announceWinner function that announces the Winner once either the PC or User has gotten a total score of 3

--- .idea/script.js
+++ .idea/script.js
@@ -1,23 +1,76 @@
 /// Pseudo Code to be found in the Readme.md !!!
+let userScore = 0;
+let pcScore = 0;
 
 
-
-function generateComputerChoice() {
+function generatePcChoice() {
     // Generates a Random number between 1 and 3.
     let numChoice = Math.floor(Math.random() * 3) + 1;
 
     switch(numChoice) {
-        case 1: return strChoice = "Rock";
+        case 1: return strChoice = "Rock"
         break;
         case 2: return strChoice = "Paper";
         break;
         case 3: return strChoice = "Scissors";
     }
+
 }
-console.log(generateComputerChoice());
 
 function getUserChoice() {
     let strChoice = prompt("Enter your choice [Rock/Paper/Scissors]");
     return strChoice;
 }
-console.log(getUserChoice());
+
+function checkSpelling() {
+    let strChoice = getUserChoice();
+    if (strChoice != "Rock" || strChoice != "Paper" || strChoice != "Scissors") {
+        strChoice.toLowerCase()
+        const userChoice = strChoice.charAt(0).toUpperCase() + strChoice.slice(1);
+        return userChoice;
+    } else {
+        return strChoice;
+
+    }
+}
+
+function checkWinner() {
+    const userChoice = checkSpelling();
+    console.log("User: " + userChoice);
+    const pcChoice = generatePcChoice();
+    console.log("PC: " + pcChoice);
+
+    if(userChoice === pcChoice){
+        console.log("It's a tie!")
+    } else if (
+        (userChoice === "Rock" && pcChoice === "Scissors") ||
+        (userChoice === "Paper" && pcChoice === "Rock") ||
+        (userChoice === "Scissors" && pcChoice === "Paper")
+    ) {
+        console.log("You win!")
+        userScore++;
+    } else {
+        console.log("Computer wins!")
+        pcScore++;
+    }
+    return;
+}
+
+function announceWinner() {
+    if (userScore === 3) {
+        console.log("User Wins! You had a score of " + userScore + " and the PC had a score of " + pcScore);
+    } else {
+        console.log("You lost! You had a score of " + userScore + " and the PC had a score of " + pcScore);
+    }
+    return;
+}
+
+function playGame() {
+    while (userScore != 3 && pcScore != 3) {
+        checkWinner();
+        console.log("User Score: " + userScore + " Pc Score: " + pcScore);
+    }
+    announceWinner();
+}
+
+playGame();
